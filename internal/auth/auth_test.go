362bf681934a5b1eff6819d0c260a5cb202bb443
saubuny <saubuny@gmail.com>
2024-09-13 12:34:23 -0400

    create tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	headerPass := make(http.Header, 0)
+	headerPass.Add("Authorization", "ApiKey blahblahblah")
+	got, err := GetAPIKey(headerPass)
+	want := "blahblahblah"
+	if err != nil {
+		t.Fatalf("expected: %v, got: %v", want, err.Error())
+	}
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+
+	t.Fatalf("Force Fail")
+}
